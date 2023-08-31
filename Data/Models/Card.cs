6e3a342032810ce862335b18fa6ea71ffb12df22
Konstantin Reshalov <adm.iz.otdela.fsb@gmail.com>
2023-08-31 18:31:54 +0300

    feature Create model for Card and CardManager for manged cards collection

---
+++ Data/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsTools.Data.Models
+{
+    internal class Card
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
