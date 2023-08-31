54143a73f18c68fc4383f89b732474f5cfd11580
Konstantin Reshalov <adm.iz.otdela.fsb@gmail.com>
2023-08-31 16:54:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Serilog.Sinks.SystemConsole.Themes;
+using Serilog;
+
+namespace CardsTools
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+        
+    }
+}
